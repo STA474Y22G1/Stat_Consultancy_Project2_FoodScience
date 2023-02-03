c730600c4f485f0f4ae76a3c7cab42c25ab05e7b
ThisaakhyaJ <thisaakhya@gmail.com>
2023-02-03 16:22:54 +0530

    PCA dataset

---
+++ Data_filtering.R
@@ -0,0 +1,18 @@
+oildata<-read_csv("Tidy Data.csv")
+
+
+oildata <- rename(oildata, Concentration = `Palm olein concentration(C)`, 
+                  Replicate = `Replicate No`, W=`Wave Number (cm-1)(W)`, A=`Absorption (A)`)
+
+# Selecting VCO and adulterated
+filterdata1<-oildata %>% filter(Series==c("Pure VCO", "Adulterated" ))
+
+# Filtering Wavelenghts
+filterdata2<-filterdata1 %>% filter(W>=3000 & W<=3010)
+filterdata3<-filterdata1 %>% filter(W>=1650 & W<=1660)
+filterdata4<-filterdata1 %>% filter(W>=1105 & W<=1120)
+
+#PCA
+PCAdata<-rbind(filterdata2, filterdata3, filterdata4)
+
+write_csv(PCAdata,"PCAData.csv")
