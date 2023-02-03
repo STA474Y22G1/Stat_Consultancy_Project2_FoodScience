47ba79052aa26168127b7c927d32452f5cda62b6
trishiii <wickramarathnetrishika@gmail.com>
2023-02-03 23:00:52 +0530

    Bartlett and LDA

--- LDA script.R
+++ LDA script.R
@@ -2,17 +2,33 @@
 
 #Libraries
 library(tidyverse)
+library(MASS)
 
 # load the data
 LDA_data <- read_csv("PCAData.csv")
-
+view(LDA_data)
 
 # Normality Test
 df1 <- LDA_data %>% filter(Series == "Pure VCO")
+view(df1)
 #df1 <- df1 %>% rename(var1 = `3010`)
 
-shapiro.test(df1$var1)
+#shapiro.test(df1$var1)
 shapiro.test(df1$`3009`)
 
-# var-cov matrix
-df2 <- df1 %>% select(-c("Index", "Series", "Concentration", "Replicate"))
+
+
+#check for group homogenity - Bartlett test
+result = bartlett.test(`3002` ~Series , 
+                       data = LDA_data)
+result
+
+
+
+#LDA
+df2 <- LDA_data%>% select(-c("Index",  "Concentration", "Replicate"))
+view(df2)
+lda_results <- lda(Series~., df2)
+lda_results
+
+
