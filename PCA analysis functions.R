fe1fe652dac770c83fcc3de45f49376a460d5bec
ThisaakhyaJ <thisaakhya@gmail.com>
2023-02-05 11:50:39 +0530

    dataet edition

--- PCA analysis functions.R
+++ PCA analysis functions.R
@@ -1,5 +1,5 @@
 # Importing Data set
-oildata<-read_csv("Tidy Data.csv") # This is for training set
+dataset<-read_csv("Tidy Data.csv") # This is for training set
 
 #####################################################################################################
 
@@ -85,7 +85,7 @@ Data_Analysis<-function(dataset){
 
 #######################################################################################################
 
-Data_Analysis(oildata)
+Data_Analysis(dataset)
 
 
 
